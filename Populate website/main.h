6bce537ad211f6a4a8c0c570a38acd43e65ff443
Serena <serena.f.yu@gmail.com>
2017-12-13 14:31:03 +1100

    Collect references
    Now it collects reference information

--- Populate website/main.h
+++ Populate website/main.h
@@ -13,8 +13,3 @@ namespace std
 		}
 	};
 }
-
-void readFile(char* path, std::unordered_map<std::string, int>& geneid, std::unordered_map<std::pair<int, int>, int>& record);
-void populateDB(std::unordered_map<std::string, int>& geneid, std::unordered_map<std::pair<int, int>, int>& record);
-void insertID(int tid, std::pair<std::string, int> const& gene, _ConnectionPtr& pConn);
-void insertRecord(int tid, std::pair<std::pair<int, int>, int> const& item, _ConnectionPtr& pConn);
